46090160cef53f53c89659b966741927a5562905
Vladislav Sabolotny <vladislav.sabolotny@gmail.com>
2025-06-02 22:10:48 +0200

    added api tests

--- backend/config.py
+++ backend/config.py
@@ -9,7 +9,7 @@ class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'you_should_change_this'
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or f'sqlite:///{db_path}'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    JWT_SECRET_KEY = os.environ.get('JWT_SECRET_KEY') or 'your_jwt_secret_key_please_change'
+    JWT_SECRET_KEY = os.environ.get('JWT_SECRET_KEY') or 'ba5d152a234ae39176e5d1ed04e3396ab0188e4264c08d2db36ddb10449075ef'
     # Define UPLOAD_FOLDER; adjust 'app/static/uploads' as per your actual structure
     # This assumes UPLOAD_FOLDER is backend/app/static/uploads
     UPLOAD_FOLDER = os.path.join(basedir, 'app', 'static', 'uploads') 
